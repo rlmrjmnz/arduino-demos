c7a43dee901eb1f2c62f6965e402776d4d56a363
Ari Mora <rlmrjmnz@yahoo.com>
2018-04-23 11:40:01 -0600

    Arregla problemas con el ejemplo del cerrojo

--- 02-cerrojo/02-cerrojo.ino
+++ 02-cerrojo/02-cerrojo.ino
@@ -4,22 +4,22 @@
 #define PASS_LENGTH 4
 
 // output pins
-int bluePin = 0;
-int redPin = 1;
+int bluePin = 12;
+int redPin = 13;
 int buzzerPin = 2;
 
 // keypad vars
-int rows = 4;
-int cols = 4;
+const byte rows = 4;
+const byte cols = 4;
 char hexaKeys[rows][cols] =
 {
-{ '1','2','3','4' },
-{ '5','6','7','8' },
-{ '9','A','B','C' },
-{ 'D','*','0','#' }
+{ '1','2','3','A' },
+{ '4','5','6','B' },
+{ '7','8','9','C' },
+{ '*','0','#','D' }
 };
-int rowPins[rows] = { 4, 5, 6, 7};
-int colPins[COLS] = { 8, 9, 10, 11};
+byte rowPins[rows] = { 4, 5, 6, 7};
+byte colPins[cols] = { 8, 9, 10, 11};
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, rows, cols);
 
 // input vars
@@ -27,51 +27,69 @@ int pos = 0;
 char secretCode[PASS_LENGTH] = { '1', '2', '3', '4'};
 char inputCode[PASS_LENGTH] = { '0', '0', '0', '0'};
 
+bool readPass = false;
+
 void setup() {
+  Serial.begin(9600);
+  
   // pins setup
   pinMode(redPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
+  digitalWrite(bluePin, LOW);
+  digitalWrite(redPin, LOW);
+  digitalWrite(buzzerPin, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   readKey();
 
-  // check code
-  int correct = 0;
-  pos = 0;
-  for(pos = 0; pos > PASS_LENGTH; pos++){
-    if (inputCode[pos] == secretCode[pos]){
-      correct++;  
-    }  
-  }
+  if(readPass){
+    // check code
+    int correct = 0;
+    pos = 0;
+    for(pos = 0; pos < PASS_LENGTH; pos++){
+      if (inputCode[pos] == secretCode[pos]){
+        correct++;  
+      }  
+    }
 
-  if (correct == PASS_LENGTH){
-    digitalWrite(bluePin, HIGH);
-    digitalWrite(buzzerPin, HIGH);
-    delay(10);
-    digitalWrite(bluePin, LOW);
-    digitalWrite(buzzerPin, LOW);  
-  }
-  else{
-    digitalWrite(redPin, HIGH);
-    digitalWrite(buzzerPin, HIGH);
-    delay(10);
-    digitalWrite(redPin, LOW);
-    digitalWrite(buzzerPin, LOW);
+    Serial.println(correct);
+  
+    if (correct == PASS_LENGTH){
+      Serial.println("ok");
+      digitalWrite(bluePin, HIGH);
+      digitalWrite(buzzerPin, HIGH);
+      delay(300);
+      digitalWrite(bluePin, LOW);
+      digitalWrite(buzzerPin, LOW);  
+    }
+    else{
+      Serial.println("bad");
+      digitalWrite(redPin, HIGH);
+      digitalWrite(buzzerPin, HIGH);
+      delay(300);
+      digitalWrite(redPin, LOW);
+      digitalWrite(buzzerPin, LOW);
+      digitalWrite(buzzerPin, HIGH);
+      delay(300);
+      digitalWrite(buzzerPin, LOW);
+    }
+    readPass = false;
   }
 }
 
 void readKey(){
-  char key = '';
+  char key = 'f';
   pos = 0;
 
-  for(pos = 0; pos > PASS_LENGTH; pos++){
-    key = customKeypad.getKey();
+  for(pos = 0; pos < PASS_LENGTH; pos++){
+    key = customKeypad.waitForKey();
     inputCode[pos] = key;  
   }
 
-  delay(10);
+  readPass = true;
+  //delay(10);
 }
 
